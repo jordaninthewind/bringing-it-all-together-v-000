c664ab75ab1c11f6beb9e59beb2386c855f040cc
Jordan Kline <jordan.kline@gmail.com>
2018-02-01 00:48:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,7 +44,7 @@ attr_reader :id
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    dog_hash = {:id = result[0], :name = result[1], :breed = result[2]}
+    dog_hash = {id: result[0], name: result[1], breed: result[2]}
     binding.pry
     self.new()
   end

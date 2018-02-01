7d009e1bcac4ea595bd10cdc715b88b0e2a9ce67
Jordan Kline <jordan.kline@gmail.com>
2018-02-01 00:57:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,9 +63,16 @@ attr_reader :id
     self.new(dog_hash)
   end
 
-  def self.find_or_create_by
-
-  end
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
 
   def self.new_from_db
 

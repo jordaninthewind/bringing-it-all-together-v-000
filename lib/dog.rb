a72924927babdc9882860988d461ab892b68955e
Jordan Kline <jordan.kline@gmail.com>
2018-02-01 01:08:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,7 @@ attr_reader :id
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).flatten
     if !dog.empty?
-      # dog_hash = {id: dog[0], name: dog[1], breed: dog[2]}
+      dog_hash = {id: dog[0], name: dog[1], breed: dog[2]}
       dog = Dog.new(dog_hash)
     else
       dog = self.create({name: name, breed: breed})
@@ -76,6 +76,6 @@ attr_reader :id
 
   def self.new_from_db(row)
     hash = {id: row[0], name: row[1], breed: row[2]}
-    hash
+    self.new(hash)
   end
 end
